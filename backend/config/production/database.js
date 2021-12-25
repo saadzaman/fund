daabc09c0f58ed740e9b3fe74ba877cd2e4f67b0
Saad Zaman <saad.zaman@outlook.com>
2021-12-26 01:09:12 +0500

    setup DB

--- backend/config/production/database.js
+++ backend/config/production/database.js
@@ -1,11 +1,30 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: 'ec2-3-217-216-13.compute-1.amazonaws.com',
+      port: 5432,
+      database: 'ddgbhkaju2jfoe',
+      user: 'uksfxkaztyoeon',
+      password: '33941ec48928e52fa13d230d39bb1a88753640d034145206463c2971bc7bed50',
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
+ 
