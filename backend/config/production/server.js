49a24e2f3a770d3eecadd932483ef2c6602abae3
Saad Zaman <saad.zaman@outlook.com>
2021-12-26 02:23:03 +0500

    remove portts

--- backend/config/production/server.js
+++ backend/config/production/server.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  port: process.env.PORT,
   host: '0.0.0.0',
   url: 'https://hamdardpk.herokuapp.com',
 });
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
   url: 'https://hamdardpk.herokuapp.com',
-  port: process.env.PORT,
   host: '0.0.0.0',
 });
