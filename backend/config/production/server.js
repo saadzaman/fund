6ba759e9ae2983d9b1e4d130183b29f7c7dce46e
Saad Zaman <saad.zaman@outlook.com>
2021-12-26 01:41:58 +0500

    try config change

--- backend/config/production/server.js
+++ backend/config/production/server.js
@@ -1,3 +1,5 @@
 module.exports = ({ env }) => ({
+  port: process.env.PORT,
+  host: '0.0.0.0',
   url: 'https://hamdardpk.herokuapp.com',
 });
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,3 +1,5 @@
 module.exports = ({ env }) => ({
   url: 'https://hamdardpk.herokuapp.com',
+  port: process.env.PORT,
+  host: '0.0.0.0',
 });
